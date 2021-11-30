9b948c42950c851ca687f4f599244bbfff47c43e
Piotrek Wysocki <garry@aikido.art.pl>
2021-11-30 02:23:42 +0100

    change example

--- demo.php
+++ demo.php
@@ -5,19 +5,17 @@ use SimpleNN\Layer;
 require_once "vendor/autoload.php";
 
 $X = [
-    [1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 1],
-    [1, 0, 1, 0, 1],
-    [1, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1],
+    [1, 2, 3, 2.5],
+    [2, 5, -1, 2],
+    [-1.5, 2.7, 3.3, -0.8],
 ];
 
 srand(0);
 
-$layer1 = new Layer(5, 10);
-$layer2 = new Layer(10, 2);
+$layer1 = new Layer(4, 5);
+$layer2 = new Layer(5, 2);
 
 $layer1->forward($X);
 $layer2->forward($layer1->getOutput());
 
-var_dump($layer2->getOutput());
+echo new \NumPHP\Core\NumArray($layer1->getOutput());

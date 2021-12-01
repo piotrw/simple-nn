40e79bfac0865f0a8acc964686e82b081134a89e
Piotrek Wysocki <garry@aikido.art.pl>
2021-12-01 23:48:10 +0100

    rename methods

--- src/Tools/Matrix.php
+++ src/Tools/Matrix.php
@@ -7,19 +7,36 @@ use NumPHP\Core\NumArray;
 class Matrix
 {
     /**
-     * Adds vector to Matrix
+     * Adds row vector to Matrix
+     *
      * @param array|NumArray $matrix
      * @param array|NumArray $vector
      * @return array
      */
-    public static function addVector($matrix, $vector): array
+    public static function addRowVector($matrix, $vector): array
+    {
+        if(!$matrix instanceof NumArray) {
+            $matrix = new NumArray($matrix);
+        }
+
+        return $matrix->add($vector)->getData();
+    }
+
+    /**
+     * Adds column vector to Matrix
+     *
+     * @param array|NumArray $matrix
+     * @param array|NumArray $vector
+     * @return array
+     */
+    public static function addColumnVector($matrix, $vector): array
     {
         if($matrix instanceof NumArray) {
             $matrix = $matrix->getData();
         }
 
-        return array_map(function ($input) use ($vector) {
-            $np = new NumArray($input);
+        return array_map(function ($row) use ($vector) {
+            $np = new NumArray($row);
             return $np->add($vector)->getData();
         }, $matrix);
     }

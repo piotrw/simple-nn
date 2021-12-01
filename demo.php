d2f6bc2845a2b7ef60ace29319387c5bbb689c33
Piotrek Wysocki <garry@aikido.art.pl>
2021-12-01 23:58:36 +0100

    demo update

--- demo.php
+++ demo.php
@@ -1,5 +1,7 @@
 <?php
 
+use NumPHP\Core\NumArray;
+use SimpleNN\Activation;
 use SimpleNN\Layer;
 
 require_once "vendor/autoload.php";
@@ -16,6 +18,13 @@ $layer1 = new Layer(4, 5);
 $layer2 = new Layer(5, 2);
 
 $layer1->forward($X);
-$layer2->forward($layer1->getOutput());
 
-echo new \NumPHP\Core\NumArray($layer1->getOutput());
+$activation1 = new Activation\ReLU();
+$activation2 = new Activation\ReLU(); // todo SoftPlus
+
+$activation1->forward($layer1->getOutput());
+$layer2->forward($activation1->getOutput());
+
+$activation2->forward($layer2->getOutput());
+
+echo new NumArray($activation2->getOutput());
